0083a5f93dab54f3cbf24037ddb97c3d3812b196
lyabomyr <lyabomyr@gmail.com>
2021-02-03 23:08:07 +0200

    No find element, need add expection

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,21 @@
+from selenium.webdriver import Remote as RemoteWebDriver
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
-        # ваша реализация
+        self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except ("Login link is not presented"):
+            return False
+        return True
